6b943120e168f9d0392e0971bc9b6ba256865871
Chaedonghyun <102035495+Chaedonghyun@users.noreply.github.com>
2024-03-12 14:28:07 +0900

    서버연결
    서버연결

--- frontend/index.js
+++ frontend/index.js
@@ -14,43 +14,45 @@ const calcTime = (timestamp) => {
 const renderData = (data) => {
   const main = document.querySelector("main");
 
-  data.sort().forEach(async (obj) => {
-    const div = document.createElement("div");
-    div.className = "items-list";
+  data
+    .sort((a, b) => a.b)
+    .forEach(async (obj) => {
+      const div = document.createElement("div");
+      div.className = "items-list";
 
-    const imgDiv = document.createElement("div");
-    imgDiv.className = "item-list__img";
+      const imgDiv = document.createElement("div");
+      imgDiv.className = "item-list__img";
 
-    const img = document.createElement("img");
-    const res = await fetch(`/images/${obj.id}`);
-    const blob = await res.blob();
-    const url = URL.createObjectURL(blob);
-    img.src = url;
+      const img = document.createElement("img");
+      const res = await fetch(`/images/${obj.id}`);
+      const blob = await res.blob();
+      const url = URL.createObjectURL(blob);
+      img.src = url;
 
-    const InfoDiv = document.createElement("div");
-    InfoDiv.className = "item-list__info";
+      const InfoDiv = document.createElement("div");
+      InfoDiv.className = "item-list__info";
 
-    const InfoTitleDiv = document.createElement("div");
-    InfoTitleDiv.className = "item-list__info-title";
-    InfoTitleDiv.innerText = obj.title;
+      const InfoTitleDiv = document.createElement("div");
+      InfoTitleDiv.className = "item-list__info-title";
+      InfoTitleDiv.innerText = obj.title;
 
-    const InfoMetaDiv = document.createElement("div");
-    InfoMetaDiv.className = "item-list__info-meta";
-    InfoMetaDiv.innerText = obj.place + " " + calcTime(obj.insertAt);
+      const InfoMetaDiv = document.createElement("div");
+      InfoMetaDiv.className = "item-list__info-meta";
+      InfoMetaDiv.innerText = obj.place + " " + calcTime(obj.insertAt);
 
-    const InfoPriceDiv = document.createElement("div");
-    InfoPriceDiv.className = "item-list__info-price";
-    InfoPriceDiv.innerText = obj.price;
+      const InfoPriceDiv = document.createElement("div");
+      InfoPriceDiv.className = "item-list__info-price";
+      InfoPriceDiv.innerText = obj.price;
 
-    imgDiv.appendChild(img);
+      imgDiv.appendChild(img);
 
-    InfoDiv.appendChild(InfoTitleDiv);
-    InfoDiv.appendChild(InfoMetaDiv);
-    InfoDiv.appendChild(InfoPriceDiv);
-    div.appendChild(imgDiv);
-    div.appendChild(InfoDiv);
-    main.appendChild(div);
-  });
+      InfoDiv.appendChild(InfoTitleDiv);
+      InfoDiv.appendChild(InfoMetaDiv);
+      InfoDiv.appendChild(InfoPriceDiv);
+      div.appendChild(imgDiv);
+      div.appendChild(InfoDiv);
+      main.appendChild(div);
+    });
 };
 const fetchList = async () => {
   const res = await fetch("/items");
