e88932ea135b9ca1e03666317e4dc1938506b99c
Chaedonghyun <102035495+Chaedonghyun@users.noreply.github.com>
2024-03-12 16:04:01 +0900

    로그인
    로그인 html,js,py 추가

---
+++ frontend/signup.js
@@ -0,0 +1,36 @@
+const form = document.getElementById("signup-form");
+
+const checkPassword = () => {
+  const formData = new FormData(form);
+  const password1 = formData.get("password");
+  const password2 = formData.get("password2");
+  if (password1 == password2) {
+    return true;
+  } else return false;
+};
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const sha256Password = sha256(formData.get("password"));
+  formData.set("password", sha256Password);
+  console.log(formData.get("password"));
+
+  const div = document.getElementById("info");
+  if (checkPassword()) {
+    const res = await fetch("/signup", {
+      method: "POST",
+      body: formData,
+    });
+    const data = await res.json();
+    if (data == "200") {
+      div.innerText = "회원가입 성공했습니다.";
+      div.style.color = "blue";
+    }
+  } else {
+    div.innerText = "비밀번호를 확인하세요";
+    div.style.color = "red";
+  }
+};
+
+form.addEventListener("submit", handleSubmit);
